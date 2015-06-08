ad651b2435a292542a1529eba5d7cffd4724186e
Martin <martin.koterle@gmail.com>
2015-06-08 21:24:46 +0200

    Logo changes.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,7 @@ About - twitchls
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="/">twitc<span class="logo-blue">h</span>ls</a>
+              <a class="navbar-brand" href="/"><span class="logo">twitc<span class="blue">hls</span></span></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               
@@ -31,7 +31,7 @@ About - twitchls
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 
-                <h1>About twitc<span class="logo-blue">h</span>ls</h1>
+                <h1>About <span class="logo">twitc<span class="blue">hls</span></span></h1>
 
                 <p>This is an open source project, enabling you to watch streams using the <a href="http://en.wikipedia.org/wiki/HTTP_Live_Streaming">HLS</a> techonolgy instead of Flash. It uses significantly less power on a Mac. It also has the option to display the Twitch chat, which does need Flash to load (the CPU usage for the Flash chat is insignificant compared to the video streaming).
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@ Stream list - twitchls
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="/">twitc<span class="logo-blue">h</span>ls</a>
+              <a class="navbar-brand" href="/"><span class="logo">twitc<span class="blue">hls</span></span></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               

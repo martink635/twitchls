9d611a97e132c893ecc2d4d863f245a95c4380ff
Martin Koterle <martin.koterle@gmail.com>
2015-07-07 22:37:06 +0200

    Added Vue.js.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ elixir(function(mix) {
     mix.sass('app.scss')
        .scripts([
            '../../../node_modules/jquery/dist/jquery.js',
+           '../../../node_modules/vue/dist/vue.js',
            '../../../node_modules/bootstrap/dist/js/bootstrap.js',
            'app.js'
        ], './public/js/app.js');

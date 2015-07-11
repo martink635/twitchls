9c279775870eaefdd5738e69119cb32501d79a1e
Martin Koterle <martin.koterle@gmail.com>
2015-07-12 01:18:20 +0200

    Added some functional tests.

---
+++ tests/FunctionalTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class FunctionalTest extends TestCase
+{
+    public function testPageLoads()
+    {
+        $this->visit('/')
+             ->see('twitc');
+    }
+
+    public function testApiStreams()
+    {
+        $this->visit('/api/v1/streams/50/0')
+             ->seeJson();
+    }
+
+    public function testApiGames()
+    {
+        $this->visit('/api/v1/games/5')
+             ->seeJson();
+    }
+}

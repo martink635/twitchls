eaa44217df8998780d9accbc814cd4aa28735e8e
Martin Koterle <martin.koterle@gmail.com>
2015-07-12 11:11:44 +0200

    Added baseUrl to tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,13 @@
 class TestCase extends Laravel\Lumen\Testing\TestCase
 {
     /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+    
+    /**
      * Creates the application.
      *
      * @return \Laravel\Lumen\Application

b34a7269511a8120df1ad723870565e010fa00d6
Martin Koterle <martin.koterle@gmail.com>
2016-03-16 12:24:21 +0100

    Added keydown events on stream view.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir')
 
-elixir(function(mix) {
-    mix.sass('app.scss')
-       .browserify('app.js', './public/js/app.js')
-       .browserify('stream.js', './public/js/stream.js');
-});
+elixir(function (mix) {
+  mix.sass('app.scss')
+    .browserify('app.js', './public/js/app.js')
+    .browserify('stream.js', './public/js/stream.js')
+})

3b74f4816f05cc10b81f1063777a94f5d1ff281d
Martin Koterle <martin.koterle@gmail.com>
2016-03-16 13:49:56 +0100

    Updated Elixir.

--- resources/assets/js/stream.js
+++ resources/assets/js/stream.js
@@ -1,33 +1,54 @@
-'use strict';
+'use strict'
 
-var browser = require('./modules/browser');
+var browser = require('./modules/browser')
 
-browser.ready(function() {
+browser.ready(function () {
+  var keyDown = false
+  var hideChat = document.getElementById('hide-chat')
+  var fullscreen = document.getElementById('fullscreen')
 
-    var hideChat = document.getElementById('hide-chat');
-
-    if (hideChat) {
-        hideChat.addEventListener('click', function(e) {
-            this.childNodes[0].classList.toggle('glyphicon-indent-right');
-            this.childNodes[0].classList.toggle('glyphicon-indent-left');
-            this.parentNode.classList.toggle('btn-group-on-stream');
-
-            document.getElementById('chat').classList.toggle('hidden');
-            document.getElementById('chat').classList.toggle('col-md-3');
-
-            document.getElementById('stream').classList.toggle('col-md-9');
-            document.getElementById('stream').classList.toggle('col-md-12');
-        }, false);
+  document.body.addEventListener('keydown', (event) => {
+    if (keyDown) {
+      return
     }
 
-    var fullscreen = document.getElementById('fullscreen');
+    keyDown = true
 
-    if (fullscreen) {
-        fullscreen.addEventListener('click', function(e) {
-            browser.toggleFullScreen();
-            this.childNodes[0].classList.toggle('glyphicon-resize-full');
-            this.childNodes[0].classList.toggle('glyphicon-resize-small');
-        }, false);
+    switch (event.keyCode) {
+      case 37:
+      case 39:
+        toggleChat()
+        break
+      case 40:
+      case 38:
+        toggleFullscreen()
+        break
     }
+  })
 
-});
+  document.body.addEventListener('keyup', () => { keyDown = false })
+
+  hideChat.addEventListener('click', toggleChat, false)
+  fullscreen.addEventListener('click', toggleFullscreen, false)
+
+  function toggleChat () {
+    var chatDiv = document.getElementById('chat')
+    var streamDiv = document.getElementById('stream')
+
+    hideChat.childNodes[0].classList.toggle('glyphicon-indent-right')
+    hideChat.childNodes[0].classList.toggle('glyphicon-indent-left')
+    hideChat.parentNode.classList.toggle('btn-group-on-stream')
+
+    chatDiv.classList.toggle('hidden')
+    chatDiv.classList.toggle('col-md-3')
+
+    streamDiv.classList.toggle('col-md-9')
+    streamDiv.classList.toggle('col-md-12')
+  }
+
+  function toggleFullscreen () {
+    browser.toggleFullScreen()
+    fullscreen.childNodes[0].classList.toggle('glyphicon-resize-full')
+    fullscreen.childNodes[0].classList.toggle('glyphicon-resize-small')
+  }
+})

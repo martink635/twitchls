2d8fe811da68e87796f1fb1883091d1ce4e98219
Martin <martin.koterle@gmail.com>
2023-11-15 08:59:29 +0100

    Package updates & removal of analytics

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-    @include('analytics')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -43,26 +42,26 @@
     <div class="w-full h-1 bg-teal-500"></div>
 
     @if (env('APP_ENV') !== 'production')
-    <div class="absolute left-0 right-0 flex justify-center w-full">
-        <a href="{{ route('preview') }}"
-            class="px-3 py-2 text-xs font-bold uppercase transition duration-100 bg-teal-500 rounded-b-lg hover:bg-teal-900">Preview
-            version</a>
-    </div>
+        <div class="absolute left-0 right-0 flex justify-center w-full">
+            <a href="{{ route('preview') }}"
+                class="px-3 py-2 text-xs font-bold uppercase transition duration-100 bg-teal-500 rounded-b-lg hover:bg-teal-900">Preview
+                version</a>
+        </div>
     @endif
 
-    <header class="flex flex-col justify-between max-w-6xl px-4 py-10 mx-auto sm:px-6 sm:flex-row">
+    <header class="flex flex-col justify-between max-w-6xl px-4 py-10 mx-auto sm:flex-row sm:px-6">
         <a href="{{ route('home') }}">
             <x-logo /></a>
 
         <div class="flex mt-4 space-x-4 sm:mt-0">
             <x-primary href="{{ route('about') }}">About</x-primary>
             @guest
-            <x-primary href="{{ route('login') }}">Log In with Twitch.tv</x-primary>
+                <x-primary href="{{ route('login') }}">Log In with Twitch.tv</x-primary>
             @endguest
 
             @auth
-            <x-primary href="{{ route('settings') }}">Settings</x-primary>
-            <x-primary href="{{ route('logout') }}">Log out</x-primary>
+                <x-primary href="{{ route('settings') }}">Settings</x-primary>
+                <x-primary href="{{ route('logout') }}">Log out</x-primary>
             @endauth
         </div>
 
@@ -73,7 +72,7 @@
     </main>
 
     <footer
-        class="flex flex-col justify-between max-w-6xl px-4 mx-auto mt-16 mb-8 space-y-4 text-sm text-gray-500 sm:px-6 sm:space-y-0 sm:flex-row sm:mt-32 sm:mb-16">
+        class="flex flex-col justify-between max-w-6xl px-4 mx-auto mt-16 mb-8 space-y-4 text-sm text-gray-500 sm:mb-16 sm:mt-32 sm:flex-row sm:space-y-0 sm:px-6">
         <a href="{{ route('home') }}">
             <x-logo /></a>
         <span>Alternative Twitch.tv listing with chat. Available on <x-link
